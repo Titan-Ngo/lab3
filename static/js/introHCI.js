d4e9b6d12e83a0158bb4fae86abad28f58aedd9c
Titan-Ngo <ttngo@ucsd.edu>
2020-01-23 10:18:42 -0800

    heroku

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,29 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+
+		$('#testjs').text("Please wait...");
+
+		$(".jumbotron p").toggleClass("active");
 	});
+
+	 $(".project").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+
+	// prevent page from loading
+	e.preventDefault();
+
+	var containingProject = $(this).closest(".project");
+
+	 var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(containingProject).fadeToggle();
+    }
 }
